a5b63fcd5994af86a7e0783700b33657a74ea270
Phani Kumar Kolla <phani.kollas@gmail.com>
2022-08-25 10:35:25 +0530

    FactorialIterative file

---
+++ Mathematics/Factorial/FactorialIterative.java
@@ -0,0 +1,25 @@
+import java.io.*;
+import java.util.*;
+
+class FactorialIterative {
+
+	
+	static int fact(int n)
+	{
+		int res = 1;
+
+		for(int i=2; i<=n; i++)
+		{
+			res = res * i;
+		}
+		return res;
+	}
+
+	public static void main (String[] args) {
+		
+		int number = 5;
+
+		System.out.println(fact(number));
+
+	}
+}
