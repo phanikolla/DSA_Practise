80acd31191037eae49d5554d116cf83290a86fd2
Phani Kumar Kolla <phani.kollas@gmail.com>
2022-08-25 10:38:45 +0530

    EuclideanOptimised Algorithm

---
+++ Mathematics/GCD/EuclideanOptimised.java
@@ -0,0 +1,22 @@
+import java.io.*;
+import java.util.*;
+
+class EuclideanOptimised {
+
+	
+	static int gcd(int a, int b)
+	{
+		if(b==0)
+			return a;
+
+		return gcd(b, a % b);
+	}
+
+	public static void main (String[] args) {
+		
+		int a = 12, b = 15;
+
+		System.out.println(gcd(a, b));
+
+	}
+}
