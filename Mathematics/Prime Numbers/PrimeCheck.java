95ec64cdfd5f50722a870f2f20d87f0dd0f39493
Phani Kumar Kolla <phani.kollas@gmail.com>
2022-08-25 10:41:03 +0530

    PrimeCheck Efficient way

---
+++ Mathematics/Prime Numbers/PrimeCheck.java
@@ -0,0 +1,28 @@
+import java.io.*;
+import java.util.*;
+
+class PrimeCheck {
+
+	
+	static boolean isPrime(int n)
+	{
+		if(n==1)
+			return false;
+
+		for(int i=2; i*i<=n; i++)
+		{
+			if(n % i == 0)
+				return false; 
+		}
+
+		return true;
+	}
+
+	public static void main (String[] args) {
+		
+		int n = 65;
+
+		System.out.println(isPrime(n));
+
+	}
+}
