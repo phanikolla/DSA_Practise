16a72b4628b7bd0fb0222459bfb641189d55ff32
Phani Kumar Kolla <phani.kollas@gmail.com>
2022-08-25 10:44:28 +0530

    EfficientPrimeFactors Efficient Method

---
+++ Mathematics/Prime Factors/EfficientPrimeFactors.java
@@ -0,0 +1,36 @@
+
+import java.io.*;
+import java.util.*;
+
+class EfficientPrimeFactors {
+
+	
+	static void printPrimeFactors(int n)
+	{
+		if(n <= 1)
+			return;
+
+		for(int i=2; i*i<=n; i++)
+		{
+			while(n % i == 0)
+			{
+				System.out.print(i+" ");
+
+				n = n / i;
+			}
+		}
+
+		if(n > 1)
+			System.out.print(n+" ");
+
+		System.out.println();
+	}
+
+	public static void main (String[] args) {
+		
+		int n = 450;
+
+		printPrimeFactors(n);
+
+	}
+}
