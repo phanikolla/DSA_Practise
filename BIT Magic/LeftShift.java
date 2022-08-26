af3f00c4bda00e945d486a5a45da7574a168e52c
Phani Kumar Kolla <phani.kollas@gmail.com>
2022-08-26 11:06:45 +0530

    LeftShift

---
+++ BIT Magic/LeftShift.java
@@ -0,0 +1,18 @@
+import java.io.*;
+
+class LeftShift {
+	public static void main (String[] args) {
+	    
+	    int x = 3;
+	    
+	    System.out.println(x << 1);
+	    
+	    System.out.println(x << 2);
+	    
+	    int y = 4;
+	    
+	    int z = (x << y);
+	    
+	    System.out.println(z);
+	}
+}
