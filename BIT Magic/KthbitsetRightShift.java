3718bf5bcb7a4a63cc7353da251192358ea9f6ed
Phani Kumar Kolla <phani.kollas@gmail.com>
2022-08-26 11:45:11 +0530

    KthbitsetRightShift

---
+++ BIT Magic/KthbitsetRightShift.java
@@ -0,0 +1,21 @@
+import java.io.*; 
+
+class KthbitsetRightShift 
+{ 
+static void isKthBitSet(int n, 
+						int k) 
+{ 
+	if (((n >> (k - 1)) & 
+			1) == 1) 
+		System.out.println("SET"); 
+	else
+		System.out.println("NOT SET"); 
+} 
+
+// Driver code 
+public static void main (String[] args) 
+{ 
+	int n = 5, k = 1; 
+	isKthBitSet(n, k); 
+} 
+} 
