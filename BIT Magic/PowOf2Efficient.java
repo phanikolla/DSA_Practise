424196e67cf20cd5007f9e44f2e8654bc9b37d2a
Phani Kumar Kolla <phani.kollas@gmail.com>
2022-08-26 13:29:16 +0530

    PowOf2Efficient - More Efficient

---
+++ BIT Magic/PowOf2Efficient.java
@@ -0,0 +1,17 @@
+import java.io.*;
+
+class PowOf2Efficient {
+    
+    static boolean isPow2(int n)
+    {
+        if(n == 0)
+            return true;
+        
+        return ((n & (n - 1)) == 0);
+    }
+	public static void main (String[] args) {
+	    
+	    int n = 4;
+		System.out.println(isPow2(n));
+	}
+}
