c0260bbc348d7fce72bf848334456cc114e7f781
Phani Kumar Kolla <phani.kollas@gmail.com>
2022-08-29 14:38:41 +0530

    Powerset - To generate subsets

---
+++ BIT Magic/Powerset.java
@@ -0,0 +1,30 @@
+import java.util.*;
+import java.io.*;
+import java.lang.*;
+class Powerset 
+{ 
+    static void printPowerSet(String str)
+    {
+    	int n = str.length();
+
+    	int powSize = (int)Math.pow(2, n);
+
+    	for(int counter = 0; counter < powSize; counter++)
+    	{
+    		for(int j = 0; j < n; j++)
+    		{
+    			if((counter & (1 << j)) != 0)
+    				System.out.print(str.charAt(j));
+    		}
+    		
+    		System.out.println();
+    	}
+    }
+
+    public static void main(String args[]) 
+    { 
+        String s = "abc";
+
+        printPowerSet(s);  
+    } 
+}
