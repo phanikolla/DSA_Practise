6e904688249dbf33f6dcfbebbe04b85f7d4ab27a
Phani Kumar Kolla <phani.kollas@gmail.com>
2022-09-01 21:44:02 +0530

    RecExample 1

---
+++ Recursion/RecExample.java
@@ -0,0 +1,30 @@
+import java.io.*;
+import java.util.*;
+
+class RecExample {
+
+	
+	static void fun1()
+	{
+		System.out.println("fun1");
+	}
+
+	static void fun2()
+	{
+		System.out.println("Before fun1");
+
+		fun1();
+
+		System.out.println("After fun1");
+	}
+
+	public static void main (String[] args) {
+		
+		System.out.println("Before fun2");
+
+		fun2();
+
+		System.out.println("After fun2");
+
+	}
+}
