14543e3693dfe4680d9b927b873c0eafb81e2fa5
Phani Kumar Kolla <phani.kollas@gmail.com>
2022-09-01 21:47:05 +0530

    DecToBinary Conversion

---
+++ Recursion/DecToBinary.java
@@ -0,0 +1,21 @@
+import java.io.*;
+import java.util.*;
+
+class DecToBinary {
+	
+	static void fun(int n)
+	{
+		if(n == 0)
+			return;
+		
+		fun(n / 2);
+
+		System.out.println(n % 2);
+
+	}
+    public static void main(String [] args) 
+    {
+        fun(7);
+    }
+
+}
