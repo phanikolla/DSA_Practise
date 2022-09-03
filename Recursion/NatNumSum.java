101bada832a46730a99f6b521684c29a4edda12f
Phani Kumar Kolla <phani.kollas@gmail.com>
2022-09-03 07:39:27 +0530

    NatNumSum

---
+++ Recursion/NatNumSum.java
@@ -0,0 +1,23 @@
+import java.io.*;
+import java.util.*;
+
+class NatNumSum {
+
+	
+	static int getSum(int n)
+	{
+		if(n == 0)
+			return 0;
+
+		return n + getSum(n - 1);
+
+	}
+    public static void main(String [] args) 
+    {
+    	int n = 4;
+    	
+    	System.out.println(getSum(n));
+        
+    }
+
+}
