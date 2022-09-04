cc5fbcbde63a139c6d43ad8639682c4df1f9c063
Phani Kumar Kolla <phani.kollas@gmail.com>
2022-09-04 09:39:43 +0530

    SumOfDigits

---
+++ Recursion/SumOfDigits.java
@@ -0,0 +1,21 @@
+import java.io.*;
+import java.util.*;
+
+class SumOfDigits {
+
+	
+	static int fun(int n)
+	{
+		if(n < 10)
+			return n;
+
+		return fun(n / 10) + n % 10;
+	}
+    public static void main(String [] args) 
+    {
+    	
+    	System.out.println(fun(253));
+        
+    }
+
+}
