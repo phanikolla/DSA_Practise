ac7f5751579127f818db8e12c41268761b3c9276
Phani Kumar Kolla <phani.kollas@gmail.com>
2022-09-15 12:16:29 +0530

    MaximumDiff - Naive approach

---
+++ Arrays/MaximumDiff.java
@@ -0,0 +1,30 @@
+import java.util.*;
+import java.io.*;
+import java.lang.*;
+class MaximumDiff 
+{ 
+    static int maxDiff(int arr[], int n)
+    {
+    	int res = arr[1] - arr[0];
+
+    	for(int i = 0; i < n - 1; i++)
+    	{
+    		for(int j = i + 1; j < n; j++)
+    		{
+    			res = Math.max(res, arr[j] - arr[i]);
+    		}
+    	}
+
+    	return res;
+    }
+
+
+    public static void main(String args[]) 
+    { 
+       int arr[] = {2, 3, 10, 6, 4, 8, 1}, n = 7;
+
+       System.out.println(maxDiff(arr, n));
+
+    } 
+
+}
